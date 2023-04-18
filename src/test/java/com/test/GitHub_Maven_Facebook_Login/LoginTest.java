5b0a2b3ee4c9a6b3e97aad73a392ba56e8966754
unknown <snehanagrare5@gmail.com>
2023-04-18 10:19:01 +0530

    Initial commit

---
+++ src/test/java/com/test/GitHub_Maven_Facebook_Login/LoginTest.java
@@ -0,0 +1,54 @@
+package com.test.GitHub_Maven_Facebook_Login;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void InitialStep() {
+		System.setProperty("webdriver.chrome.driver","C:\\Slenium_Drivers\\chromedriver_win32 (3)\\chromedriver.exe\\");
+		 driver = new ChromeDriver();
+System.out.println("Launching Browser");	
+	}
+	@Test
+	public void doLogin() {
+	System.out.println("Executing Login Test");	
+		driver.get("https://en-gb.facebook.com/");
+		
+	driver.manage().timeouts().implicitlyWait(20L, TimeUnit.SECONDS);
+	
+	System.out.println("Entering Username");
+	driver.findElement(By.xpath("//*[@id='email']")).sendKeys("snehanagrare5@gmail.com");
+	
+	System.out.println("Entering Password");
+	driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("9673523195");
+	
+	System.out.println("Clicking on Login Button");
+	driver.findElement(By.name("login")).click();
+	
+	System.out.println("Logging into facebook account");
+	}
+	
+    @AfterTest
+public void CloseBrowser() {
+    	if(driver!=null)
+    		driver.close();
+    }
+	}
+	
+	
+	
+	
+	
+	
+	
+
